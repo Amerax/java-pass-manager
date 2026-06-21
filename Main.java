3740a1fec2653f84981519f62b65ca2c3dc6e145
Raiyan K <96327738+Amerax@users.noreply.github.com>
2026-06-21 19:39:55 -0400

    Refactor password storage and user interaction
    Refactored password management functionality to store passwords in a static HashMap and updated user prompts.

--- Main.java
+++ Main.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 
 public class Main{
     static Scanner sc = new Scanner(System.in);
+    static HashMap<String, Password> passList = new HashMap<>();
     public static void main(String[] args){
-        HashMap<String, Password> passList = new HashMap<>();
 
         System.out.println("Hello welcome to java password manager!");
         System.out.println("All of this runs locally without a connection to the internet needed! Superbly safe :O");
@@ -14,8 +14,8 @@ public class Main{
         while(running){
             System.out.println("""
                     0: Close program
-                    1: Enter new password info
-                    2: Access pass info 
+                    1: Save a new password log
+                    2: Access saved pass info 
                     """);
                 
             int input = sc.nextInt();
@@ -28,6 +28,33 @@ public class Main{
                 default -> System.out.println("Invalid selection");
             }
         }
+    }
 
+    public void newInfo(){
+        System.out.println("Enter the website name you would like to save a password to: ");
+        String website = sc.nextLine();
+        System.out.println("Enter the username");
+        String user = sc.nextLine();
+        System.out.println("Enter the password");
+        String pass = sc.nextLine();
+
+        Password created = new Password(user, pass, website);
+
+        passList.put(website, created);
+
+        System.out.println("Would you like to put in optional security question information? (y/n)");
+        String input = sc.nextLine();
+        if(input.equals("y")){
+            System.out.println("Enter the security question");
+            created.setSecQuestion(sc.nextLine());
+            System.out.println("Enter the security question answer");
+            created.setSecAns(sc.nextLine());
+        }
+
+        System.out.println("Password information succesfully stored!");
+    }
+
+    public void access(){
+        
     }
 }
